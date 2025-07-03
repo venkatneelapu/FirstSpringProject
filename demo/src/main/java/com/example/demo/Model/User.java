db87616947c3086f53cebf821ecce06b50892de6
venkatneelapu <neelapu.venkatanaidu@gmail.com>
2025-07-03 10:41:42 +0530

    updated entity by adding timestamp

--- demo/src/main/java/com/example/demo/Model/User.java
+++ demo/src/main/java/com/example/demo/Model/User.java
@@ -4,10 +4,23 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
 public class User {
+    //New: Creation Timestamp
+    @Column(name="created at",updatable = false)//updatable = false ensures it's set only once
+    @CreationTimestamp
+    private LocalDate createdAt;
+    //new: Updated timestamp
+    @Column(name ="updated At")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
     @Id //specifies the primary key of the entity
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,6 +32,22 @@ public class User {
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 
     public User(){
 
@@ -56,4 +85,5 @@ public class User {
                 ", email='" + email + '\'' +
                 '}';
     }
+
 }
